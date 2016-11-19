142c518f43d254c3b9adbefa9c3d1cc193e4806b
hampos <hampos@me.com>
2016-11-19 15:10:00 +0200

    Add Restboy.Core project, add BaseViewModel with INotifyPropertyChanged impl

---
+++ Restboy.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Restboy.Core
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged<T>(Expression<Func<T>> property)
+		{
+			var propertyInfo = ((MemberExpression)property.Body).Member as PropertyInfo;
+			if (propertyInfo == null)
+			{
+				throw new ArgumentNullException(nameof(property), "Argument is not a valid Property");
+			}
+			OnPropertyChanged(propertyInfo.Name);
+		}
+
+		protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
